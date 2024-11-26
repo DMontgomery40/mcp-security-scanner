0925f2afbcba3089b21bd6f0aa18e08102544fb5
David Montgomery <130489651+DMontgomery40@users.noreply.github.com>
2024-11-26 11:52:39 -0700

    Add complete project structure and files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SecurityScanner from './components/SecurityScanner'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <SecurityScanner />
+    </div>
+  )
+}
+
+export default App
